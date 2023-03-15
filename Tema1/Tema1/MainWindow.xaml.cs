84f4b37f79077c50c854281e50c3bfa0c41b70ac
rdmc10 <radu.m10@protonmail.com>
2023-03-15 02:18:16 +0200

    Added images for users and scroll buttons for images

--- Tema1/Tema1/MainWindow.xaml.cs
+++ Tema1/Tema1/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,25 @@ namespace Tema1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<BitmapImage> bitmapImages { get; set; }
+        private int Index { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            bitmapImages = new List<BitmapImage>()
+            {
+               new BitmapImage(new Uri(@"Resources/beard.png",UriKind.Relative)),
+               new BitmapImage(new Uri(@"Resources/dog.png",UriKind.Relative)),
+               new BitmapImage(new Uri(@"Resources/hacker.png",UriKind.Relative)),
+               new BitmapImage(new Uri(@"Resources/indian-man.png",UriKind.Relative)),
+               new BitmapImage(new Uri(@"Resources/man.png",UriKind.Relative)),
+               new BitmapImage(new Uri(@"Resources/panda.png",UriKind.Relative)),
+               new BitmapImage(new Uri(@"Resources/user.png",UriKind.Relative)),
+               new BitmapImage(new Uri(@"Resources/user2.png",UriKind.Relative)),
+            };
+            Index = 0;
+            ImageHolder.Source = bitmapImages.FirstOrDefault();
         }
 
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -30,14 +47,31 @@ namespace Tema1
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Previous_Image_Click(object sender, RoutedEventArgs e)
+        {
+            Index = (Index - 1) == -1 ? bitmapImages.Count -1 : (Index - 1);
+            ImageHolder.Source = bitmapImages[Index];
+        }
+
+        private void Next_Image_Click(object sender, RoutedEventArgs e)
+        {
+            Index = (Index + 1) % bitmapImages.Count;
+            ImageHolder.Source = bitmapImages[Index];
+        }
+
+        private void New_User_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Delete_User_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
