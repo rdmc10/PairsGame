0ee91fdca2bb8cffc2b0659075ddc2ea447cfb56
rdmc10 <radu.m10@protonmail.com>
2023-03-17 22:44:18 +0200

    Assigned random images for every button

--- Tema1/Tema1/GameWindow.xaml.cs
+++ Tema1/Tema1/GameWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,6 +27,9 @@ namespace Tema1
         private ushort LevelNumber { get; set; }
         private string Username { get; set; }
         private string PathToImg { get; set; }
+
+        private Dictionary<Tuple<ushort,ushort>,string> assignedPhoto { get; set; }
+        private List<ushort> allPhotos { get; set; }
         public GameWindow(string username, string pathToImg)
         {
             Username = username;
@@ -35,6 +40,20 @@ namespace Tema1
             NumberOfRows = 5;
             NumberOfCols = 5;
             UserImageHolder.Source = new BitmapImage(new Uri(PathToImg, UriKind.Absolute));
+            assignedPhoto = new Dictionary<Tuple<ushort,ushort>,string>();
+            allPhotos= new List<ushort>();
+            for(ushort i = 1; i < 10; i++)
+            {
+                allPhotos.Add(i);
+            }
+            for(ushort row = 0; row < NumberOfRows; row++)
+            {
+                for(ushort col = 0; col < NumberOfCols; col++)
+                {
+                    Tuple <ushort,ushort> pos = new Tuple<ushort,ushort>(row, col);
+                    assignedPhoto[pos] = "0";
+                }
+            }
         }
         private void NewGameClick(object sender, RoutedEventArgs e)
         {
@@ -51,7 +70,7 @@ namespace Tema1
                     Button button = new Button();
                     button.Content = new Image
                     {
-                        Source = new BitmapImage(new Uri(@"Images/black.jpg", UriKind.Relative)) ,
+                        Source = new BitmapImage(new Uri(@"Images/0.jpg", UriKind.Relative)) ,
                         VerticalAlignment = VerticalAlignment.Center,
                         Stretch = Stretch.Fill,
                         Height = 256,
@@ -62,9 +81,66 @@ namespace Tema1
                     matrixGrid.Children.Add(button);
                 }
             }
+            RandomAssingPhoto();
         }
 
         private void RandomAssingPhoto()
+        {
+            Random rnd = new Random();
+            foreach(Button btn in matrixGrid.Children)
+            {
+                int randomIndex = rnd.Next(1, allPhotos.Count);
+                ushort randomKey = allPhotos[randomIndex];
+
+                ushort row = (ushort)Grid.GetRow(btn);
+                ushort col = (ushort)Grid.GetColumn(btn);
+                Tuple<ushort, ushort> pos = new Tuple<ushort, ushort>(row, col);
+                List<Tuple<ushort,ushort>> falseKeys = assignedPhoto.Where(pair => pair.Value == 0.ToString()).Select(pair => pair.Key).ToList();
+                if(assignedPhoto[pos] == "0")
+                {
+                    assignedPhoto[pos] = randomKey.ToString();
+                    //TODO: Assign another unsigned button the same key;
+                    //List<Tuple<ushort,ushort>> falseKeys = assignedPhoto.Where(pair => pair.Value == 0).Select(pair => pair.Key).ToList();
+                    int rndNextIndex = rnd.Next(0, falseKeys.Count);
+                    Console.WriteLine(rndNextIndex);
+                    if(falseKeys.Count > 0)
+                    {
+                        Tuple<ushort,ushort> randomNextPos = falseKeys[rndNextIndex];
+                        assignedPhoto[randomNextPos] = randomKey.ToString();
+                    }
+                }
+            }
+            foreach(Button btn in matrixGrid.Children)
+            {
+                ushort row = (ushort)Grid.GetRow(btn);
+                ushort col = (ushort)Grid.GetColumn(btn);
+                Tuple<ushort, ushort> pos = new Tuple<ushort, ushort>(row, col);
+                btn.Content = new Image
+                {
+                    Source = new BitmapImage(new Uri(@"Images/" + assignedPhoto[pos] +".jpg", UriKind.Relative)) ,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    Stretch = Stretch.Fill,
+                    Height = 256,
+                    Width = 256
+                };
+            }
+        }
+
+        private void SaveGameClick(object sender, RoutedEventArgs e)
+        {
+            string filepath = @"Saves/" + Username + ".txt";
+            using(var sw = new StreamWriter(filepath, true))
+            {
+                foreach(Button btn in matrixGrid.Children)
+                {
+                    Image buttonImage = btn.Content as Image;
+                    BitmapSource bitmapSource = buttonImage.Source as BitmapSource;
+                    string imagePath = (bitmapSource!= null) ? bitmapSource.ToString() : null;
+                    sw.WriteLine(imagePath);
+                }
+            }
+        }
+        private void OpenGameClick(object sender, RoutedEventArgs e)
         {
 
         }
@@ -81,10 +157,6 @@ namespace Tema1
         {
 
         }
-        private void SaveGameClick(object sender, RoutedEventArgs e)
-        {
-
-        }
         private void StatisticsClick(object sender, RoutedEventArgs e)
         {
 
@@ -95,10 +167,6 @@ namespace Tema1
 
         }
 
-        private void OpenGameClick(object sender, RoutedEventArgs e)
-        {
-
-        }
 
     }
 }
